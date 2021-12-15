b5172cd896d07577edd39dc7f09f0ac86d73b085
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2021-12-16 00:32:36 +0300

    v1.0.2.1
    Güncellenenler
    ----------------
    - Business Layer - CustomerBusiness eklendi.
    
    - Views - Customer - AddCustomer ve ListCustomer eklendi.
    
    - Controllers - CustomerControllers eklendi.

---
+++ Araç_Kiralama_Otomasyonu/Araç_Kiralama_Otomasyonu/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Araç_Kiralama_Otomasyonu.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        CustomerBusiness customerBusiness = new CustomerBusiness();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customerBusiness.AddCustomer(customer);
+            return RedirectToAction("ListCustomer");
+        }
+        public ActionResult ListCustomer()
+        {
+            var Customers = customerBusiness.GetCustomerList();
+            return View(Customers);
+        }
+    }
+}
---
+++ Araç_Kiralama_Otomasyonu/BusinessLayer/Concrete/CustomerBusiness.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CustomerBusiness
+    {
+        GenericRepository<Customer> repo = new GenericRepository<Customer>();
+        public List<Customer> GetCustomerList()
+        {
+            return repo.List();
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            repo.Delete(customer);
+        }
+        public void AddCustomer(Customer customer)
+        {
+            repo.Insert(customer);
+        }
+        public void CustomerUpdate(Customer customer)
+        {
+            repo.Update(customer);
+        }
+    }
+}
