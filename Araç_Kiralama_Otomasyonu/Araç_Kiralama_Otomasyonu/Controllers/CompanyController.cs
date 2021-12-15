182fff8b42725b004e980ecb2253eb579515140c
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2021-12-15 23:29:22 +0300

    v1.0.2
    Güncellenenler
    ----------------
    - Business Layer - CompanyBusiness eklendi.
    
    - Views - Company - AddCompany ve ListCompany eklendi.
    
    - Controllers - CompanyControllers eklendi.

---
+++ Araç_Kiralama_Otomasyonu/Araç_Kiralama_Otomasyonu/Controllers/CompanyController.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Araç_Kiralama_Otomasyonu.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        CompanyBusiness companyBusiness = new CompanyBusiness();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddCompany()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCompany(Company company)
+        {
+            companyBusiness.AddCompany(company);
+            return RedirectToAction("ListCompany");
+        }
+        public ActionResult ListCompany()
+        {
+            var Companies = companyBusiness.GetCompanyList();
+            return View(Companies);
+        }
+    }
+}
---
+++ Araç_Kiralama_Otomasyonu/BusinessLayer/Concrete/CompanyBusiness.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CompanyBusiness
+    {
+        GenericRepository<Company> repo = new GenericRepository<Company>();
+        public List<Company> GetCompanyList()
+        {
+            return repo.List();
+        }
+        public void DeleteCompany(Company company)
+        {
+            repo.Delete(company);
+        }
+        public void AddCompany(Company company)
+        {
+            repo.Insert(company);
+        }
+        public void CompanyUpdate(Company company)
+        {
+            repo.Update(company);
+        }
+    }
+}
